a6345cb47ed26f57458391ce0a009a71fb7af341
VictorTheDuck <49794608+VictorTheDuck@users.noreply.github.com>
2021-06-30 00:22:59 -0500

    custom message to flood

--- amogusReview.py
+++ amogusReview.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import random
 import time
+message = input('What would you like to flood? > ')
 while True:
     driver = webdriver.Chrome()
     driver.get('https://www.popsilla.com/com.innersloth.spacemafia?utm_source=bing&utm_medium=cpc&utm_campaign=PS%20-%20AU%20-%20Desktop&utm_term=among%20us&utm_content=Among%20Us')
@@ -8,7 +9,7 @@ while True:
     time.sleep(1)
     commentBox = driver.find_element_by_xpath('//*[@id="hypercomments_widget"]/div/div[2]/div[4]/div/div[2]/textarea')
     commentBox.click()
-    commentBox.send_keys('STOP POSTING ABOUT AMONG US !!!!!!')
+    commentBox.send_keys(message)
 
     addCommentButton = driver.find_element_by_xpath('//*[@id="hypercomments_widget"]/div/div[2]/div[4]/div/div[5]/div[2]') 
     addCommentButton.click()
